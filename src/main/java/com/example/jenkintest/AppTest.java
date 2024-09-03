3ac43d8ad01427ac3ed514aa7a1175f39d5d519b
AdwaidDineshIntern <163625542+AdwaidDineshIntern@users.noreply.github.com>
2024-09-03 17:36:35 +0530

    Update AppTest.java

--- src/main/java/com/example/jenkintest/AppTest.java
+++ src/main/java/com/example/jenkintest/AppTest.java
@@ -1,15 +1,13 @@
 package com.example.jenkintest;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
 
     @Test
-    public void testAdd() {
-        App app = new App();
-        assertEquals(5, app.add(2, 3));
+    public void testSum() {
+        int result = 2 + 3;
+        assertEquals(5, result);
     }
 }
-
